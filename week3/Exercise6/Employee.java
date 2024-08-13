a279692bcae7a04f8945057ca629443804d41091
Bharanidharan K <146436593+Bharani-15@users.noreply.github.com>
2024-08-13 11:50:55 +0530

    Add files via upload

---
+++ week3/Exercise6/Employee.java
@@ -0,0 +1,25 @@
+package com.example.EmployeeManagementSystem.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@NamedQuery(name = "Employee.findByEmail", query = "SELECT e FROM Employee e WHERE e.email = :email")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+}

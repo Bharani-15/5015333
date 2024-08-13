316d1c4c72f48741a6f18de85f5d13b3c9b21eeb
Bharanidharan K <146436593+Bharani-15@users.noreply.github.com>
2024-08-13 10:16:39 +0530

    Add files via upload

---
+++ week2/Exercise9_Spring/BookController.java
@@ -0,0 +1,22 @@
+package com.example.LibraryManagement.controller;
+
+import com.example.LibraryManagement.entity.Book;
+import com.example.LibraryManagement.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}

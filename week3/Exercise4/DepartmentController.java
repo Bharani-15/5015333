f62ba4f790e175c273310c625d3c9344b367d98c
Bharanidharan K <146436593+Bharani-15@users.noreply.github.com>
2024-08-13 11:39:36 +0530

    Add files via upload

---
+++ week3/Exercise4/DepartmentController.java
@@ -0,0 +1,44 @@
+package com.example.EmployeeManagementSystem.controller;
+
+import com.example.EmployeeManagementSystem.entity.Department;
+import com.example.EmployeeManagementSystem.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/departments")
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @GetMapping
+    public List<Department> getAllDepartments() {
+        return departmentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Department> getDepartmentById(@PathVariable Long id) {
+        Department department = departmentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Department not found"));
+        return ResponseEntity.ok(department);
+    }
+
+    @PostMapping
+    public Department createDepartment(@RequestBody Department department) {
+        return departmentRepository.save(department);
+    }
+    
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDepartment(@PathVariable Long id) {
+        Department department = departmentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Department not found"));
+
+        departmentRepository.delete(department);
+        return ResponseEntity.noContent().build();
+    }
+}

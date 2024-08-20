e567e7ef01e851d5f0f395e45fb349407e563ce8
Bharanidharan K <146436593+Bharani-15@users.noreply.github.com>
2024-08-20 20:31:30 +0530

    Add files via upload

---
+++ week4/Exercise7/CustomerDTO.java
@@ -0,0 +1,56 @@
+package com.example.BookstoreAPI.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class CustomerDTO {
+
+    private Long id;
+
+    @NotBlank(message = "First name is mandatory")
+    @Size(max = 50, message = "First name must be less than 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    @Size(max = 50, message = "Last name must be less than 50 characters")
+    private String lastName;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

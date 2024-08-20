54b9d7ad6aff3b4dcbbd256b58ad010641e0c77e
Bharanidharan K <146436593+Bharani-15@users.noreply.github.com>
2024-08-20 20:45:38 +0530

    Add files via upload

---
+++ week4/Exercise8/Customer.java
@@ -0,0 +1,36 @@
+package com.example.BookstoreAPI.customer;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Version;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.Id;
+
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 50)
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @Version
+    private Long version;
+
+    // Getters and setters
+}

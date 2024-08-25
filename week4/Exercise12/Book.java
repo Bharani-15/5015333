410b5ba0c108a45e71d1786b921c9fe66bcb2427
Bharanidharan K <146436593+Bharani-15@users.noreply.github.com>
2024-08-25 20:20:47 +0530

    Add files via upload

---
+++ week4/Exercise12/Book.java
@@ -0,0 +1,24 @@
+package com.example.BookStoreAPI.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private Double price;
+    private String isbn;
+}

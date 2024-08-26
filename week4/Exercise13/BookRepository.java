cb8a1b8f2f2896e88080eb9781cbbf06f95680e5
Bharanidharan K <146436593+Bharani-15@users.noreply.github.com>
2024-08-26 10:22:23 +0530

    Delete week4/Exercise13 directory

--- week4/Exercise13/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.BookStoreAPI.repository;
-
-import com.example.BookStoreAPI.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {}
